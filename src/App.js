27c8d1e5fa7a177265bd8bc612eeed07b4835a8b
Nav Volcy <navvolcy@gmail.com>
2024-04-14 12:04:31 -0600

    finished buliding the Home and About page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import About from './components/About';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Home/>
+      <About/>
     </div>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 
-function About (){
+const About = () =>{
+
     return ( 
-        <div>About</div>
+        <div
+        name="about"
+         id="about"
+         className="w-full h-screen bg-[#0a192f] text-gray-300"
+         >
+            <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className="py-16 rounded-md bg-cyan-800 flex flex-col juistify-center items-center w-4/6">
+                    <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
+                        <div className="sm:text-right pb-8 pl-4">
+                            <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
+                                About
+                            </p>
+                        </div>
+                        <div></div>
+                    </div>
+                    <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+                        <div className="sm:text-right text-4xl font-bold">
+                            <p>
+                                Hi. I'm Nav L. Volcy, thank you for visiting my web page and 
+                                take a look around.
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                Nav,
+                                A software developer with experience in building Responsive and
+                                Scalable Web apps. I have knowledge in UI/UX principles. Coupled with 
+                                good communication skills. In additoin to understanding the technologies needed to 
+                                build web apps.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
     )
 }
 
